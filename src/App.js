faa747be46248ede6f41a7e1f6e568e7c6350b5c
divergent98 <ljiljanagospic@gmail.com>
2023-10-25 22:52:43 +0200

    Google map loaded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { useJsApiLoader, GoogleMap, Marker } from "@react-google-maps/api";
+import { useState } from "react";
+
+const center = { lat: 41.8818, lng: -87.623 };
 
 function App() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"],
+  });
+
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  if (!isLoaded) {
+    return <div>ne radi</div>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        position: "relative",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          left: 0,
+          top: 0,
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+          options={{
+            zoomControl: false,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false,
+          }}
+          onLoad={(map) => setMap(map)}
         >
-          Learn React
-        </a>
-      </header>
+          <Marker position={center} />
+        </GoogleMap>
+      </div>
     </div>
   );
 }
