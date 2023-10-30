1a150d89b047a264c00292763bcbae78080c6269
divergent98 <ljiljanagospic@gmail.com>
2023-10-30 22:08:07 +0100

    Adding swal and styling

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import {
   DirectionsRenderer,
 } from "@react-google-maps/api";
 import { useState, useRef } from "react";
+import Swal from "sweetalert2";
+import "font-awesome/css/font-awesome.min.css";
 
 const center = { lat: 41.902782, lng: 12.496366 };
 
@@ -23,6 +25,7 @@ function App() {
   const [stop, setStop] = useState([{ value: "" }]);
   const [stops, setStops] = useState([]);
   const [stopsArray, setStopsArray] = useState([]);
+  const [buttonIcon, setButtonIcon] = useState("fa-times");
   const stopRef = useRef();
   const originRef = useRef();
   const destinationRef = useRef();
@@ -35,28 +38,61 @@ function App() {
       stopover: true,
     };
   });
+  console.log(stopsArray);
+  console.log(stops);
   async function findRoute() {
-    if (originRef.current.value === "" || destinationRef.current.value === "") {
-      return;
-    }
-    // eslint-disable-next-line no-undef
-    const directionsService = new google.maps.DirectionsService();
-    const results = await directionsService.route({
-      origin: originRef.current.value,
-      destination: destinationRef.current.value,
-      waypoints: waypoints,
-
+    try {
+      if (
+        originRef.current.value === "" ||
+        destinationRef.current.value === ""
+      ) {
+        Swal.fire({
+          title: "Error!",
+          text: "Origin and Destination fields cannot be empty",
+          icon: "error",
+          confirmButtonText: "Ok",
+        });
+        return;
+      }
+      if (stopsArray.length < stops.length) {
+        Swal.fire({
+          title: "Error!",
+          text: "You must enter a valid location",
+          icon: "error",
+          confirmButtonText: "Ok",
+        });
+        return;
+      }
       // eslint-disable-next-line no-undef
-      travelMode: google.maps.TravelMode.DRIVING,
-    });
-    setDirectionsResponse(results);
-    setDivVisible(!isDivVisible);
+      const directionsService = new google.maps.DirectionsService();
+      const results = await directionsService.route({
+        origin: originRef.current.value,
+        destination: destinationRef.current.value,
+        waypoints: waypoints,
+
+        // eslint-disable-next-line no-undef
+        travelMode: google.maps.TravelMode.DRIVING,
+      });
+      setDirectionsResponse(results);
+      console.log(results);
+    } catch (error) {
+      Swal.fire({
+        title: "Couldnt geocode!",
+        text: "Please enter a valid location",
+        icon: "error",
+        confirmButtonText: "Ok",
+      });
+    }
   }
+
   const handleAddStop = () => {
     setStops([...stops, ""]);
   };
   const toggleDivVisibility = () => {
     setDivVisible(!isDivVisible);
+    setStops([]);
+    setStopsArray([]);
+    setButtonIcon(isDivVisible ? "fa-bars" : "fa-times");
   };
   function customAutocomplete(input, index) {
     const autocomplete = new window.google.maps.places.Autocomplete(input);
@@ -118,16 +154,21 @@ function App() {
           )}
         </GoogleMap>
       </div>
-      <div>      
-        <button style={{ zIndex:100, position: "relative" }} className="bg-gray-500 hover:bg-gray-600 text-white font-semibold py-1 px-2 w-20 rounded mt-2" onClick={toggleDivVisibility}>Hide</button>
-        {isDivVisible && <div className="responsive-div">
-        <div>
-      
-          </div> 
+      <div>
+        <button
+          style={{ zIndex: 100, position: "relative" }}
+          className="custom-hide-button"
+          onClick={toggleDivVisibility}
+        >
+          <i className={`fa ${buttonIcon}`}></i>
+        </button>
+
+        <div className={`responsive-div ${isDivVisible ? "show" : ""}`}>
+          <div></div>
           <div>
             <Autocomplete>
               <input
-                className="lg:p-1 lg:w-100 lg:border lg:rounded-md"
+                className="p-1 w-100 border rounded-md"
                 type="text"
                 placeholder="Origin"
                 ref={originRef}
@@ -138,20 +179,19 @@ function App() {
           <div>
             <Autocomplete>
               <input
-                className="lg:p-1 lg:w-100 lg:border lg:rounded-md mt-4"
+                className="p-1 w-100 border rounded-md mt-4"
                 type="text"
                 placeholder="Destination"
-                
                 ref={destinationRef}
               />
             </Autocomplete>
           </div>
-         
+
           {stops.map((stop, index) => (
             <div key={index}>
               <div style={{ flexGrow: 1 }}>
                 <input
-                className="lg:p-1 lg:w-100 lg:border lg:rounded-md mt-4"
+                  className="p-1 w-100 border rounded-md mt-4"
                   type="text"
                   placeholder={`Stop ${index + 1}`}
                   ref={(input) => customAutocomplete(input, index)}
@@ -159,24 +199,45 @@ function App() {
               </div>
             </div>
           ))}
-         <div>
-            <button  className="bg-gray-500 hover:bg-gray-600 text-white font-semibold py-1 px-2 w-100 rounded mt-2"  onClick={handleAddStop}>Add stop</button>
-          </div> 
           <div>
-            <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-1 px-2 w-100 rounded mt-2" onClick={findRoute}>Find Route</button>
+            <button
+              className="bg-gray-500 hover:bg-gray-600 text-white font-semibold py-1 px-2 w-100 rounded mt-2"
+              onClick={handleAddStop}
+            >
+              Add stop
+            </button>
           </div>
-        
-          <div class="flex justify-between mt-20">
-  <div>
-    <button class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded" onClick={clearRoute}>Clear Route</button>
-  </div>
-  <div>
-    <button class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded" onClick={() => { map.panTo(center); map.setZoom(5); }}>Center Back</button>
-  </div>
-</div>
+          <div>
+            <button
+              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-1 px-2 w-100 rounded mt-2"
+              onClick={findRoute}
+            >
+              Find Route
+            </button>
+          </div>
 
-        </div>}
-        
+          <div class="flex justify-between mt-20">
+            <div>
+              <button
+                class="px-4 py-2 bg-gray-500 hover:bg-gray-600  text-white font-semibold rounded"
+                onClick={clearRoute}
+              >
+                Clear Route
+              </button>
+            </div>
+            <div>
+              <button
+                class="px-4 py-2 bg-gray-500 hover:bg-gray-600  text-white font-semibold rounded"
+                onClick={() => {
+                  map.panTo(center);
+                  map.setZoom(5);
+                }}
+              >
+                Center Back
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
